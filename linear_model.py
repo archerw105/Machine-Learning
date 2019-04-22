839ed69324c67bb80da51a0f1fcbeab734f6c2b0
archerw105 <archerdwang@gmail.com>
2019-04-22 09:57:05 -0500

    code for linear regression

--- linear_model.py
+++ linear_model.py
@@ -1,9 +1,21 @@
 import numpy as np
-import scipy as sp 
+
+X = np.array([0, 1, 2])
+y = np.array([0, 1, 2])
 
 class LinearModel():
-    pass
+    def __init__(self, coeff, intercept):
+        self.coeff = coeff
+        self.intercept = intercept
+    
+    def predict(self, X):
+        pass
 
-class LinearRegression():
-    pass
+class LinearRegression(LinearModel):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+    
 
