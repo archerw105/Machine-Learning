5e2d1a0ac421dd1c3ac8122d87ed040ef3ae88f9
archerw105 <archerdwang@gmail.com>
2019-04-24 00:11:34 -0500

    Added matrix operations

--- linear_model.py
+++ linear_model.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-X = np.array([[0, 1], [1, 1], [2, 1]])
+X = np.array([[0, 1], [1, 0], [2, -1]])
 y = np.array([1, 2, 3])
 
 class LinearModel():
     """Linear model from 
         coefficient and intercept parameters"""
-
     def __init__(self):
         self.coeff = np.array([])
         self.intercept = 0
@@ -32,7 +31,8 @@ class LinearRegression(LinearModel):
     def fit(self, X, y):
         "Ax = y; A is training set appended by column of 1's; y is target vector"
         A = np.vstack([X.T, np.ones(len(X))]).T
-        m = np.linalg.lstsq(A, y, rcond = None)[0]
+        m = np.linalg.pinv((A.T).dot(A)).dot(A.T).dot(y)
+        
         self.set_coeff(m[0:len(m)-1])
         self.set_intercept(m[len(m)-1])
         
