55061fc792ce277070d7cbe4a32b2dc11e48ead7
archerw105 <archerdwang@gmail.com>
2019-04-25 08:17:06 -0500

    Perceptron

---
+++ perceptron.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+class Perception:
+    def __init__(self, input_num):
+        self.weights = np.zeros(input_num+1)
+
+    def train(self, input_t, ):
+        pass
+    
+    def predict(self, input):
+        pass
