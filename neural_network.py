2f4e768ead5fe6694107dd2004c96dbc3f2a3297
archerw105 <archerdwang@gmail.com>
2019-05-07 00:06:48 -0500

    Neural network to recognize handwritten digits

--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,7 @@
 import numpy as np
 import random
 import mnist_loader
-
+#based off of Michael Nielsen's "Neural Networks and Deep Learning"
 def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
@@ -59,10 +59,10 @@ class Network:
             for batch in mini_batches:
                 self.update_from_batch(batch, eta)
            
-        if test_data:
-            print("Epoch {} : {} / {}".format(_,self.evaluate(test_data),n_test));
-        else:
-            print("Epoch {} complete".format(_))
+            if test_data:
+                print("Epoch {} : {} / {}".format(_,self.evaluate(test_data),n_test));
+            else:
+                print("Epoch {} complete".format(_))
 
     def update_from_batch(self, batch, eta):
         """
@@ -77,7 +77,7 @@ class Network:
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         for x, y in batch:
             delta_nabla_w, delta_nabla_b = self.backprop(x, y)
-            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]#nw and dnw are matrices(?)
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]#nw and dnw are matrices
             nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
         
         self.weights = [w - eta*(nw/len(batch))
@@ -99,21 +99,22 @@ class Network:
             activation = sigmoid(z)
             activations.append(activation)
         #backward pass
-        delta = self.cost_derivative(y, activations[-1]) * sigmoid_prime(zs[-1])#Hadamard product
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])#Hadamard product
         nabla_w[-1] = np.dot(delta, activations[-2].transpose()) #creates matrix
         nabla_b[-1] = delta
         for l in range(2, self.num_layers):
             z = zs[-l]
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sigmoid_prime(z) #double-check
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sigmoid_prime(z)
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
             nabla_b[-l] = delta
 
         return (nabla_w, nabla_b)
     
     def cost_derivative(self, output, y):
+        """Cost function is set to be 1/2*(output - y)^2"""
         return (output - y)
 
-    def evaluate(self, test_data):
+    def evaluate(self, test_data):#copied
         """Return the number of test inputs for which the neural
         network outputs the correct result. Note that the neural
         network's output is assumed to be the index of whichever
@@ -122,6 +123,6 @@ class Network:
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
-# net = Network([784, 30, 10])
-# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-# net.sgd(training_data, 30, 10, 3.0, test_data=test_data)
+net = Network([784, 30, 10])
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net.sgd(training_data, 30, 10, 3.0, test_data=test_data)
